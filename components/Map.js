38295c52b26ede93e7877cdc4833e19b7ccf0dc8
Lisis <laruschel@proton.me>
2023-03-12 23:25:46 +0100

    viewport change needs to be fixed

--- components/Map.js
+++ components/Map.js
@@ -1,10 +1,10 @@
+import React, { useState } from "react";
 import ReactMapGL from "react-map-gl";
-import { useState } from "react";
 
 export default function Map() {
   const [viewport, setViewport] = useState({
-    width: "100%",
     height: "100%",
+    width: "100%",
     latitude: 52.5072,
     longitude: 13.4247,
     zoom: 8,
@@ -15,7 +15,7 @@ export default function Map() {
       mapStyle="mapbox://styles/laraujo/clf2iydy9000c01lxt1e9waqd"
       mapboxAccessToken={process.env.mapbox_key}
       {...viewport}
-      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      onViewportChange={(newViewport) => setViewport(newViewport)}
     ></ReactMapGL>
   );
 }
