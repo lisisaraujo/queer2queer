ab1523aa9f49495517ca1030c2d53608fb5b06ba
Lisis <laruschel@proton.me>
2023-03-15 17:32:48 +0100

    a

--- components/Map.js
+++ components/Map.js
@@ -2,11 +2,10 @@ import React, { useRef, useEffect, useState } from "react";
 import ReactMapGL, { Marker, Popup, openInfo } from "react-map-gl";
 import { accessToken } from "../src/mapbox";
 import { MdLocationOn } from "react-icons/md";
+import Router from "next/router";
 
 export default function Map({ locations }) {
-  const [popupInfo, setPopupInfo] = useState(null);
-  const [show, setShow] = useState(false);
-  const [selectedLocation, setSelectedLocation] = useState({});
+  const [showPopup, setShowPopup] = React.useState(true);
   const [viewport, setViewport] = useState({
     height: "100%",
     width: "100%",
@@ -15,8 +14,7 @@ export default function Map({ locations }) {
     zoom: 12,
   });
 
-  const iconStyles = { color: "pink", fontSize: "1.5em", cursor: "pointer" };
-
+  const iconStyles = { color: "white", fontSize: "1.5em" };
   return (
     <ReactMapGL
       mapStyle="mapbox://styles/dalalamad/clf5w8x0x009v01mo2feklchc"
@@ -25,36 +23,34 @@ export default function Map({ locations }) {
       onMove={(evt) => setViewport(evt.viewport)}
     >
       {locations.map((location) => {
-        <Marker
-          key={location._id}
-          longitude={parseFloat(location.lngLat[1])}
-          latitude={parseFloat(location.lngLat[0])}
-          color={location.color}
-          style={{ cursor: "pointer", zIndex: 999 }}
-        >
-          <p
-            role="icon"
-            onClick={() => setSelectedLocation(location)}
-            aria-label="push-pin"
+        return (
+          <Marker
+            key={location._id}
+            longitude={parseFloat(location.lngLat[1])}
+            latitude={parseFloat(location.lngLat[0])}
+            color={location.color}
+            style={{ cursor: "pointer", zIndex: 999 }}
+            onClick={() => Router.push("/about")}
           >
             <MdLocationOn style={iconStyles} />
-          </p>
-        </Marker>;
-        {
-          selectedLocation.name === location.name ? (
-            <Popup
-              onClose={() => setSelectedLocation({})}
-              closeOnClick={true}
-              longitude={parseFloat(location.lngLat[1])}
-              latitude={parseFloat(location.lngLat[0])}
-            >
-              {location.name}
-            </Popup>
-          ) : (
-            false
-          );
-        }
+          </Marker>
+        );
       })}
     </ReactMapGL>
   );
 }
+
+// {
+//   selectedLocation.name === location.name ? (
+//     <Popup
+//       onClose={() => setSelectedLocation({})}
+//       closeOnClick={true}
+//       longitude={location.lngLat[1]}
+//       latitude={location.lngLat[0]}
+//     >
+//       {location.name}
+//     </Popup>
+//   ) : (
+//     false
+//   );
+// }
---
+++ pages/locations/[locationID].js
@@ -0,0 +1,117 @@
+import { useRouter } from "next/router";
+import Image from "next/image";
+import CommentForm from "../../Components/CommentForm";
+import FavoriteButton from "../../Components/FavoriteButton";
+import styled from "styled-components";
+import ReturnButton from "../../Components/ReturnButton";
+
+export default function ShowPage({
+  pieces,
+  artPiecesInfo,
+  updateArtPiecesInfo,
+  onToggleFavorite,
+}) {
+  const router = useRouter();
+  const foundPiece = pieces.find((piece) => piece.slug === router.query.slug);
+  const { artist, colors, genre, dimensions, imageSource, name, year, slug } =
+    foundPiece;
+
+  const handleSubmitComment = (event) => {
+    event.preventDefault();
+    const date = new Date().toLocaleString();
+    const myFormData = new FormData(event.target);
+    const formDataObj = Object.fromEntries(myFormData.entries());
+    const commentAndDate = { ...formDataObj, date: date };
+
+    updateArtPiecesInfo((draft) => {
+      let currentPiece = draft.defaultValue.find(
+        (piece) => piece.slug === slug
+      );
+      if (!currentPiece) {
+        draft.defaultValue.push({
+          slug,
+          isFavorite: false,
+          comments: [commentAndDate],
+        });
+      } else {
+        currentPiece.comments = [commentAndDate, ...currentPiece.comments];
+      }
+    });
+    event.target.reset();
+    const input = document.getElementById("comment");
+    input.focus();
+  };
+
+  const currentInfo = artPiecesInfo.defaultValue.find((piece) => {
+    return piece.slug === slug;
+  });
+
+  return (
+    <Container>
+      <ReturnButton />
+      <ImageContainer>
+        <Image
+          src={imageSource}
+          alt="image of the day"
+          width={600}
+          height={600}
+          // fill
+        />
+      </ImageContainer>
+      <ColorsContainer>
+        {colors.map((color) => {
+          return (
+            <>
+              <ColorDiv key={color} style={{ backgroundColor: color }}>
+                {color}
+              </ColorDiv>
+            </>
+          );
+        })}
+      </ColorsContainer>
+      <section className="paintingDetails">
+        <p>Name: {name}</p>
+        <p>Artist: {artist}</p>
+        <p>Genre: {genre}</p>
+        <p>Year created: {year}</p>
+      </section>
+      <FavoriteButton slug={slug} onToggleFavorite={onToggleFavorite} />
+      <CommentForm onSubmitComment={handleSubmitComment} />
+      {currentInfo &&
+        currentInfo.comments.map((comment, index) => {
+          return (
+            <div className="commentCard" key={index}>
+              <p className="date">{comment.date}</p>
+              <p className="comment">{comment.comment}</p>
+            </div>
+          );
+        })}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ImageContainer = styled.div`
+  margin: 20px 0;
+`;
+
+const ColorsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 600px;
+`;
+
+const ColorDiv = styled.div`
+  background-color: #fff;
+  flex-grow: 1;
+  color: #000;
+  padding: 10px;
+  font-size: 0.7rem;
+  height: 30px;
+  /* color: rgb(60, 60, 60); */
+`;
