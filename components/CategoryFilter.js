d76f16aaf9a7b60b50671f328eb2dee78769e6e8
Lisis <laruschel@proton.me>
2023-03-22 09:55:59 +0100

    changed filter options order and disabled select

--- components/CategoryFilter.js
+++ components/CategoryFilter.js
@@ -1,12 +1,14 @@
 export default function CategoryFilter({ onChangeCategory }) {
   return (
     <select name="category-list" id="category-list" onChange={onChangeCategory}>
-      <option value="">Select Filter</option>
-      <option value="">All</option>
+      <option value="" disabled>
+        Select Filter
+      </option>
       <option value="Bar">Bar</option>
       <option value="Club">Club</option>
       <option value="Cruising">Cruising</option>
       <option value="Community-Center">Community-Center</option>
+      <option value="">All</option>
     </select>
   );
 }
